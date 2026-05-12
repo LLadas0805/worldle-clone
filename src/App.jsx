903d5157cdf84ecdc5b2cf6f83793129c366c321
Luke Ladas <lukeladas05@gmail.com>
2026-05-12 14:41:46 -0400

    Added randomized words

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import { useState, useEffect } from 'react'
 import './App.css'
 import Grid from './components/Grid.jsx'
+import wordList from './assets/5-letter-words.txt?raw';
+
+const words = wordList.split("\n").map(word => word.trim().toUpperCase());
+const word = words[Math.floor(Math.random() * words.length)].trim();
 
 function App() {
   // Keyword 
-  const word = "WORDS";
+  console.log(word);
 
   // React states
   const [guesses, setGuesses] = useState([])
@@ -14,6 +18,10 @@ function App() {
   useEffect (() => {
     const handleKeyDown = (event) => {
       if (guess.length === 5 && event.key === "Enter") {
+        if (!words.includes(guess)) {
+          alert("Not a valid word!");
+          return;
+        }
         const result = []
         for (let i = 0; i < word.length; i++) {
           if (guess[i] === word[i]) {
@@ -49,7 +57,7 @@ function App() {
         <div className="hero">
         </div>
         <div>
-          <h1 className="header">Wordle Clone</h1>
+          <h1 className="header">Word Gauntlet</h1>
           <Grid current = {guess} guesses={guesses} results={results}/>
         </div>
       </section>
